3adf25430286f288177a158628ead0b980dc9ef6
jingchun.xjc <jingchun.xjc@alibaba-inc.com>
2016-04-26 11:52:17 +0800

    hello world

---
+++ src/main/java/com/xjc/tools/Test.java
@@ -0,0 +1,15 @@
+/*
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2016 All Rights Reserved.
+ */
+
+package com.xjc.tools;
+
+/**
+ * Created by jingchun.xjc on 2016/4/26.11:44
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
